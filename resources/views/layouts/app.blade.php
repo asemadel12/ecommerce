7dbf70c17866de41934fa7aea0b419d7e71e02e7
asemadel12 <109317058+asemadel12@users.noreply.github.com>
2025-03-04 18:18:12 +0300

    qwe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,7 @@
     </footer>
     <!-- Bootstrap JavaScript (with Popper.js included) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!-- Bootstrap JavaScript (no Popper.js) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
